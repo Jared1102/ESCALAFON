d05118e7644f12b475990f3989c761228a8cacae
Jared <cacahuate.1895@gmail.com>
2023-09-16 11:32:55 -0600

    Merge entre la rama de Desarrollo y Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { UsuarioNuevoComponent } from './Components/usuario-nuevo/usuario-nuevo.component';
+
+const routes: Routes = [
+  {path: '',component:LoginComponent},
+  {path: 'login',component:LoginComponent},
+  {path: 'usuario-nuevo',component:UsuarioNuevoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
